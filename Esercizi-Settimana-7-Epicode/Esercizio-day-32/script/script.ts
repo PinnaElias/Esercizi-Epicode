a748b66ea2ff58bf9e07e30f54726e15432963ed
eliaspinna <eliaspinna99@gmail.com>
2024-03-06 10:15:33 +0100

    es

---
+++ Esercizi-Settimana-7-Epicode/Esercizio-day-32/script/script.ts
@@ -0,0 +1,10 @@
+class Popolo {
+    nome: String;
+    cognome: String;
+    
+    constructor(_nome: String, _cognome: string) {
+        this.nome = _nome;
+        this.cognome = _cognome;
+    }}
+    
+    let nuovaPersona = new Popolo('Gian','Franco')
