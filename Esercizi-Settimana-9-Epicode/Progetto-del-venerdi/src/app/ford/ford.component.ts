d573231dd27e78e3a01feaa477817ab81d9ce8d6
eliaspinna <eliaspinna99@gmail.com>
2024-03-22 18:34:54 +0100

    .

--- Esercizi-Settimana-9-Epicode/Progetto-del-venerdi/src/app/ford/ford.component.ts
+++ Esercizi-Settimana-9-Epicode/Progetto-del-venerdi/src/app/ford/ford.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+const jsonURL: string = '../../assets/db.json'
 
 @Component({
   selector: 'app-ford',
   templateUrl: './ford.component.html',
   styleUrls: ['./ford.component.scss']
 })
-export class FordComponent {
 
-}
+export class FordComponent implements OnInit {
+  cars: any[] = [];
+  fordCars: any[] = [];
+
+  ngOnInit(): void {
+    fetch(jsonURL)
+      .then(response => response.json())
+      .then(data => {
+        this.cars = data;
+        for (let ford of this.cars) {
+          if (data.brand === "Ford") {
+            ford.push(this.fordCars)
+          }}
+      })
+      .catch(error => {
+        console.log(error)
+      });
+}}
