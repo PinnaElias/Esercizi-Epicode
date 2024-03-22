3298814162d0fbd2276119085eb5dbfb5f398179
eliaspinna <eliaspinna99@gmail.com>
2024-03-22 16:23:15 +0100

    .

--- Esercizi-Settimana-9-Epicode/Progetto-del-venerdi/src/app/home/home.component.ts
+++ Esercizi-Settimana-9-Epicode/Progetto-del-venerdi/src/app/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+const jsonURL: string = '../../assets/db.json'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+  cars: any[] = [];
+  randomIndexA: number = 0;
+  randomIndexB: number = 0;
+
+  ngOnInit(): void {
+    fetch(jsonURL)
+      .then(response => response.json())
+      .then(data => {
+        this.cars = data;
+        this.randomIndexA = Math.floor(Math.random() * this.cars.length);
+        do {
+          this.randomIndexB = Math.floor(Math.random() * this.cars.length);
+        } while (this.randomIndexB === this.randomIndexA);
+      }
+      )
+      .catch(error => {
+        console.log(error)
+      });
+  }
 }
+
+
+
